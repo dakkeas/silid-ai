c549c1e76e50fcaca283fc834baa0c5c5369e90d
Justine Dakiss <justinedaquis28@gmail.com>
2024-04-25 20:49:32 +0800

    added login/signup functionality

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@
 console.log('firebase setup..............')
 
 
+// import { doc } from 'firebase/firestore';
 // Add Firebase products that you want to use
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js';
-import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js';
-import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js';
+import { get, getDatabase, ref, set, child} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-database.js";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -21,7 +22,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
 
 
 // firebase configuration
@@ -226,7 +226,7 @@ if (document.querySelector('.reg-container')) {
             localStorage.setItem("email", regisInfoArray[2].value);
             localStorage.setItem("password", regisInfoArray[3].value);
             localStorage.setItem("strand", selectedStrand.value);
-            window.location = 'vark.html';
+            window.open('user.html')
             
         }
     })
@@ -234,14 +234,12 @@ if (document.querySelector('.reg-container')) {
 
 }
 // NEW
-// 
+
 // modal
+
+
 const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
@@ -255,46 +253,346 @@ var span = document.getElementsByClassName("close")[0];
 // }
 
 // When the user clicks anywhere outside of the modal, close it
+const loginModal = document.querySelector('#loginModal')
+
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
 
-
-
-
-
-if (document.querySelector('.login-body')) {
-    console.log('###########LOGIN PAGE#############')
-
-    const loginPageLoginBtn = document.querySelector('.login-button button');
-    console.log(loginPageLoginBtn)
-    loginPageLoginBtn.onclick = function() {
-        console.log('login button clicked!')
-        modal.style.display = "block";
+window.onclick = function (event) {
+    if (event.target == loginModal) {
+        loginModal.style.display = "none";
     }
-
 }
 
-if (document.querySelector('.front-img-container')) {
-    // home page
-    console.log('###########HOME PAGE#############')
-    const homePageLoginBtn = document.querySelector('.header-login-container button');
-    const homePageSignUpBtn= document.querySelector('.front-signin-container button');
-    
-    homePageLoginBtn.addEventListener('click', () => {
-        console.log('hey') 
-        window.location = ('login.html')
 
+
+
+
+// if (document.querySelector('.login-body')) {
+//     console.log('###########LOGIN PAGE#############')
+
+//     const loginPageLoginBtn = document.querySelector('.login-button button');
+//     console.log(loginPageLoginBtn)
+//     loginPageLoginBtn.onclick = function() {
+//         console.log('login button clicked!')
+//         modal.style.display = "block";
+//     }
+
+// }
+
+
+if (document.querySelector('.user-page')) {
+    console.log('############# USER PAGE ##############')
+    
+    const userLogOutBtn = document.querySelector('.nav-section #user-logout');
+    const userHomeBtn = document.querySelector('.nav-section #user-go-home');
+    console.log(userLogOutBtn)
+
+    let userCreds = JSON.parse(sessionStorage.getItem('user-cred'));
+    let userInfo = JSON.parse(sessionStorage.getItem('user-info'));
+   
+
+    const nameGreetHeader = document.querySelector(".current-user-fname");
+    const pfpName = document.querySelector('#pfp-name');
+    
+    const varkSurvey = document.querySelector("#vark-questionnaire");
+    const preTest = document.querySelector("#pretest");
+    const postTest= document.querySelector("#posttest");
+    
+    
+    
+    
+    
+    userHomeBtn.onclick = () => {
+        window.location = 'home.html';
+    }
+
+    
+    userLogOutBtn.onclick = () => {
+        sessionStorage.removeItem('user-cred');
+        sessionStorage.removeItem('user-info');
+        window.location = 'home.html';
+    }
+
+    varkSurvey.onclick = () => {
+        window.location = 'vark2.html'
+    } 
+
+    postTest.onclick = () => {
+        
+        window.location = 'posttest.html'
+    } 
+    preTest.onclick = () => {
+        window.location = 'pretest.html'
+    } 
+    
+    
+    nameGreetHeader.textContent =   userInfo.fname;
+    pfpName.textContent = userInfo.fname + ' ' + userInfo.lname;
+
+    let checkCred = () => {
+        if (!sessionStorage.getItem('user-cred')) {
+            window.location = 'home.html';
+        } else {
+
+            
+            
+            
+        }
+    }
+
+    window.addEventListener('load', checkCred) 
+    
+}
+
+
+
+function userLoggedIn() {
+    if (sessionStorage.getItem('user-cred')) {
+        return true
+    } else {
+        return false
+    }
+}
+if (document.querySelector('.front-img-container')) {
+    const homePageLoginBtn = document.querySelector('.header-login-container button');
+    const homePageSignUpBtn = document.querySelector('.front-signin-container button');
+
+    if (userLoggedIn()) {
+
+        // use toggle instead
+        homePageLoginBtn.textContent = 'Log Out' 
+        homePageSignUpBtn.textContent = 'Go to Dashboard'
+        
+        homePageLoginBtn.onclick = () => {
+            sessionStorage.removeItem('user-cred');
+            sessionStorage.removeItem('user-info');
+            window.location = 'home.html';
+            
+        }
+        
+        homePageSignUpBtn.onclick = () => {
+            window.location = 'user.html';
+            
+        }
+        
+    }
+
+    // home page
+    // 
+    const auth = getAuth();
+    const db = getDatabase(); 
+    // login modal input fields
+
+    const loginEmail = document.querySelector('#loginEmail');
+    const loginPassword = document.querySelector('#loginPw');
+    const loginModalBtn = document.querySelector('.login-button button');
+    
+
+    loginModalBtn.onclick = (event) => {
+        // remove default functions
+        event.preventDefault();
+        
+        console.log('clicked on login modal!')
+        
+        if (validate_email(loginEmail.value )=== false || validate_password(loginPassword.value) === false) {
+
+            alert('invalid email/password format!')
+            return
+
+        }
+
+
+        
+        signInWithEmailAndPassword(auth, loginEmail.value, loginPassword.value)
+            .then((userCredential) => {
+                console.log('signing in');
+                const user = userCredential.user;
+                
+                console.log(user.uid)
+                
+                get(child(ref(db), 'users/' + user.uid + '/userInfo')).then((snapshot) => {
+                    // retrieving data at this instance/snapshot
+                    
+                    console.log('retrieving data!') 
+                    if(snapshot.exists()) {
+                        // if there is data!
+                        console.log('snapshot exists!') 
+                        console.log(snapshot.val())
+                        sessionStorage.setItem("user-info", JSON.stringify({
+                            // making an object from the values retrived from firebase
+                            // stringify makes the object into a string
+                            // session storage gets lost when tab is closed or redirected
+                            // saving the object into the session storage!
+                            email: snapshot.val().email,
+                            fname: snapshot.val().fname,
+                            lname: snapshot.val().lname
+                        }))
+                        
+                        sessionStorage.setItem("user-cred", JSON.stringify(user))
+                        
+                        window.location = ('user.html')
+                        
+                    } else {
+                        console.log('NO DATA FOUND!')
+                    }
+
+                    
+                })
+            })
+            .catch((error) => {
+                alert(error)
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            });
+    }
+    
+    console.log('test!')
+    console.log('###########HOME PAGE#############')
+
+    // login and signup button on home screen
+
+     
+    homePageLoginBtn.addEventListener('click', () => {
+        // clicking login button opens modal
+        // sets style to flex from 'none'
+        if (!userLoggedIn()) {
+            console.log('modal!!!!')
+            loginModal.style.display = 'flex';
+            
+        } else {
+            return
+        }
     }) 
 
     homePageSignUpBtn.addEventListener('click', () => {
-        console.log('hey') 
-        window.location = ('signup.html')
+        // navigates to signup page when signup btn is clicked
+        if (!userLoggedIn())  {
+            window.location = ('signup.html')
+        }
 
     }) 
-   
+    
+
+}
+
+// initialize variables
+
+
+
+
+
+function validate_email(email) {
+    let expression = /^[^@]+@\w+(\.\w+)+\w$/;
+
+    if (expression.test(email) == true) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function validate_password(password) {
+    if (password < 6) {
+        console.log('password is too short!')
+        return false
+    } else {
+        return true
+    }
+}
+
+
+function validate_field(field) {
+    if (field == null) {
+        return false
+    } else {
+        return true
+    }
+}
+
+
+
+if (document.querySelector('.signup-body')) {
+    console.log('######### SIGN UP PAGE #############')
+    
+
+    // input fields
+    
+    // signup btn
+    const signUpPageSignUpBtn = document.querySelector('.signup-button button');
     
     
+    // when signup btn is clicked 
+    //
+    signUpPageSignUpBtn.onclick = function(event) {
+        
+        console.log('clicked!')
+        event.preventDefault()
+        
+        const auth = getAuth();
+        const db = getDatabase();
+
+        console.log(db)
+        const signUpInputFields = document.querySelectorAll('#signup div input')
+        const signUpFName = document.querySelector("#fname")
+        const signUpLName = document.querySelector("#lname")
+        const signUpEmail = document.querySelector("#email")
+        const signUpPw = document.querySelector('#pw');
+        const signupConfirmPw = document.querySelector('#confirmpw');
+
+        if (signUpPw.value !== signupConfirmPw.value) {
+            console.log('passwords dont match!')
+            return
+        }
+        if (validate_email(signUpEmail.value) == false || validate_password(signUpPw.value) == false) {
+            console.log('invalid email or password!')
+            return
+            // dont run code anymore
+        }
+
+        if (validate_field(signUpFName.value) == false || validate_field(signUpLName.value) == false) {
+            console.log('missing input fields!')
+            return
+        }
+        console.log('creating user................')
+        createUserWithEmailAndPassword(auth, signUpEmail.value, signUpPw.value).then((userCredential) => {
+            let user = userCredential.user;
+            console.log('user created!')
+            console.log(user.uid)
+            // add this to firebase database
+            // 
+            // let database_ref = database.ref();
+
+            
+
+            set(ref(db, 'users/' + user.uid + '/userInfo'), {
+                fname: signUpFName.value,
+                lname: signUpLName.value,
+                email: signUpEmail.value,
+                last_login: Date.now()
+            })
+            // tree: users/user.uid/user_data(object)
+            // 
+
+
+            // window.location = 'home.html'
+
+        }).catch((error) => {
+            console.log('error found!')
+            let error_code = error.code;
+            let error_message = error.message;
+            console.log(error_message)
+            console.log(error_code)
+        })
+
+
+    }
+    
+    
+
+
+
 }
