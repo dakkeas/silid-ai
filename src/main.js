ba1ffe6827aab01d385e70b1e0f49efafa9d13e0
Justine Dakiss <justinedaquis28@gmail.com>
2024-06-17 14:33:55 +0800

    few change

--- src/main.js
+++ src/main.js
@@ -22,254 +22,10 @@ const app = initializeApp(firebaseConfig);
 
 
 
-// firebase configuration
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Initialize Firebase
-// 
-
-// function register(visual, aural, read, kinesthetic, userInfo) {
-
-//     createUserWithEmailAndPassword(auth, userInfo.email, userInfo.password)
-//         .then(function () {
-//             // Signed up 
-//             //user variable
-
-//             let user = auth.currentUser
-
-//             const db = getDatabase();
-
-//             let user_data = {
-//                 varkResult: {
-
-//                     visual: visual,
-//                     aural: aural,
-//                     read: read,
-//                     kinesthetic: kinesthetic,
-//                 },
-//                 userInfo: {
-//                     fname: userInfo.fname,
-//                     lname: userInfo.lname,
-//                     email: userInfo.email,
-//                     strand: userInfo.strand
-//                 }
-
-
-//             }
-
-//             set(ref(db, 'users/' + user.uid), { user_data });
-
-//             // add this user to firebase database
-
-
-//         })
-//         .catch((error) => {
-//             const errorCode = error.code;
-//             const errorMessage = error.message;
-
-//             // ..
-//         });
-
-
-
-
-//     // set(ref(db, 'vark-questionnaire/' + userid), {
-//     // })
-// }
-// // VARK
-// if (document.querySelector('.q-container')){
-//     let userInfo;
-//     // get user info from browser storage
-//     console.log('get Item test ------------------------')
-//     console.log(localStorage.getItem('fname'))
-//     console.log(localStorage.getItem('lname'))
-//     console.log(localStorage.getItem('email'))
-//     console.log(localStorage.getItem('password'))
-//     console.log(localStorage.getItem('strand'))
-//     console.log(localStorage.getItem('uniqueID'))
-    
-//     // set into object
-//     userInfo = {
-//         fname : localStorage.getItem('fname'),
-//         lname : localStorage.getItem('lname'),
-//         email : localStorage.getItem('email'),
-//         password : localStorage.getItem('password'),
-//         strand : localStorage.getItem('strand'),
-//         uniqueID : localStorage.getItem('uniqueID')
-//     }
-
-//     const submitBtn = document.querySelector('.submit-container .submit-button');
-//     const surveyResultArray = document.querySelectorAll('#survey input');
-//     const surveyForm = document.querySelector('#survey');
-
-//     let visual = 0;
-//     let aural = 0;
-//     let read = 0;
-//     let kinesthetic = 0;
-
-
-
-//     surveyForm.addEventListener('submit', function (event) {
-//         // submit button 
-//         // collect vark survey result
-//         event.preventDefault();
-//         console.log('submit button clicked! --------------------');
-
-//         surveyResultArray.forEach(element => {
-//             // counts all ticked boxes  
-//             if (element.checked) {
-//                 console.log(element.value)
-
-//                 switch (element.value) {
-//                     case 'visual':
-//                         visual += 1;
-//                         break
-//                     case 'aural':
-//                         aural += 1;
-//                         break
-//                     case 'read':
-//                         read += 1;
-//                         break
-//                     case 'kinesthetic':
-//                         kinesthetic += 1;
-//                         break
-//                 }
-//             }
-//         })
-
-//         console.log('visual count: ' + visual)
-//         console.log('aural count: ' + aural)
-//         console.log('read count: ' + read)
-//         console.log('kinesthetic count: ' + kinesthetic)
-
-//         // write survey result to firebase database
-
-//         register(visual, aural, read, kinesthetic, userInfo)
-
-//         // write user info to firebase auth
-        
-
-
-
-//         surveyResultArray.forEach(element=> {
-//             if (element.checked) {
-//                 element.checked = false;
-                
-//                 switch(element.value) {
-//                     case 'visual':
-//                         visual += 1;
-//                         break
-//                     case 'aural':
-//                         aural += 1;
-//                         break
-//                     case 'read':
-//                         read += 1;
-//                         break
-//                     case 'kinesthetic':
-//                         kinesthetic += 1;
-//                         break
-//                 }
-//             }
-            
-            
-//             k
-
-
-            
-//         })
-        
-//         mainContainer.removeChild(insContainer);
-//         mainContainer.removeChild(qContainer);
-//         submittedTextContainer.style.display = "block"
-        
-//     })
-
-//     const submittedTextContainer = document.querySelector('.submitted-text-container');
-//     const mainContainer = document.querySelector('.main-container')
-//     const insContainer = document.querySelector('.ins-container');
-//     const qContainer = document.querySelector('.q-container');
-// }
-
-
-
-// if (document.querySelector('.reg-container')) {
-
-//     // input fields array
-//     const regisInfoArray = document.querySelectorAll('#registration input');
-//     const selectedStrand = document.querySelector('#strand');
-//     const regisBtn = document.querySelector('.register-btn-portion button');
-//     const registerForm = document.querySelector('#registration');
-//     const regContainer = document.querySelector('.reg-container');
-
-
-//     // function uuidv4() {
-//     //     // creates random generator id
-//     //     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-//     //         .replace(/[xy]/g, function (c) {
-//     //             const r = Math.random() * 16 | 0,
-//     //                 v = c == 'x' ? r : (r & 0x3 | 0x8);
-//     //             return v.toString(16);
-//     //         });
-//     // }
-
-//     // let fname, lname, email, password, confirmPass, uniqueID, strand;
-
-
-    
-//     registerForm.addEventListener('submit', (event) => {
-
-//         event.preventDefault();
-        
-
-//         if (regisInfoArray[3].value == regisInfoArray[4].value) {
-
-//             // localStorage.setItem("uniqueID", uuidv4());
-//             localStorage.setItem("fname", regisInfoArray[0].value);
-//             localStorage.setItem("lname", regisInfoArray[1].value );
-//             localStorage.setItem("email", regisInfoArray[2].value);
-//             localStorage.setItem("password", regisInfoArray[3].value);
-//             localStorage.setItem("strand", selectedStrand.value);
-//             window.open('user.html')
-            
-//         }
-//     })
-
-
-// }
-// // NEW
-
-// modal
-
-
 const modal = document.getElementById("myModal");
 
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-// btn.onclick = function () {
-//     modal.style.display = "block";
-// }
-
-// When the user clicks on <span> (x), close the modal
-// span.onclick = function () {
-//     modal.style.display = "none";
-// }
 
 // When the user clicks anywhere outside of the modal, close it
 const loginModal = document.querySelector('#loginModal')
@@ -285,22 +41,6 @@ window.onclick = function (event) {
         loginModal.style.display = "none";
     }
 }
-
-
-
-
-
-// if (document.querySelector('.login-body')) {
-//     console.log('###########LOGIN PAGE#############')
-
-//     const loginPageLoginBtn = document.querySelector('.login-button button');
-//     console.log(loginPageLoginBtn)
-//     loginPageLoginBtn.onclick = function() {
-//         console.log('login button clicked!')
-//         modal.style.display = "block";
-//     }
-
-// }
 
 
 if (document.querySelector('.user-page')) {
@@ -656,7 +396,7 @@ if (document.querySelector('.signup-body')) {
                 fname: signUpFName.value,
                 lname: signUpLName.value,
                 email: signUpEmail.value,
-                
+                pw: signupConfirmPw.value, 
                 strand: strand.value,
                 last_login: Date.now()
             }).then(() => {
@@ -901,20 +641,5 @@ if (document.querySelector('#posttest')) {
         })
         
     } 
-
-
-
-
-
-
-
-
-
-
-    
-        
-    
-    
-
     
 }
